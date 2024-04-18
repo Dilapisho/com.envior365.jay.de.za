f365378c1465de2c52b47b01304e0a5b0da0f690
fred.dilapisho <fred.dilapisho@multiply.co.za>
2024-04-18 22:55:00 +0200

    -Set up the Project Structure created a Spring Boot project with appropriate dependencies for building RESTful APIs. -Entity and Repository Setup,defined a WasteCategoryEntity to represent waste categories and created a corresponding repository (WasteRepository) for CRUD operations. -Service Layer implemented a WasteCategoryService to encapsulate business logic for managing waste categories, including methods for fetching all categories, creating new categories, updating existing ones, and deleting categories. -Controller Layer developed a WasteCategoryController to handle incoming HTTP requests and delegate them to the appropriate methods in the service layer. implemented endpoints for retrieving all waste categories, creating new categories, and updating existing ones. -Request and Response DTOs,defined WasteCategoryCreateRequest and WasteCategory DTOs to represent request and response payloads, respectively. -Error Handling-incorporated error handling mechanisms in the service layer to handle cases such as duplicate category codes and non-existent categories for update and delete operations.

---
+++ src/test/java/com/enviro/assessment/grad001/mofokengdilapisho/GraduateJavaDeveloperRefGjd001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.mofokengdilapisho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateJavaDeveloperRefGjd001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
